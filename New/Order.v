0fd1665b96cce15b0af542756ffa6bd5932959ff
Eric McCorkle <emc2@metricspace.net>
2013-11-02 09:20:46 -0400

    Added definitions for positive (semi)definiteness

--- New/Order.v
+++ New/Order.v
@@ -3,7 +3,6 @@ Require Import New.Equiv.
 
 Class OrderOp A := le : A -> A -> Prop.
 
-Infix "==" := eq (at level 70, no associativity).
 Infix "<=" := le (at level 70, no associativity).
 
 Class PartialOrder A := {
@@ -18,4 +17,14 @@ Class PartialOrder A := {
 Class TotalOrder A := {
   partial_ord :> PartialOrder A;
   total_ord : forall x y : A, x <= y \/ y <= x
+}.
+
+Class PositiveSemiDefinite {A} {po : PartialOrder A} (op : A -> A) (zero : A) := {
+  positive : forall a : A, zero <= op a;
+  strict : op zero == zero
+}.
+
+Class PositiveDefinite {A} {po : PartialOrder A} (op : A -> A) (zero : A) := {
+  pos_semidef_pd :> PositiveSemiDefinite op zero;
+  zero_exclusive : forall a : A, op a == zero -> a == zero
 }.
