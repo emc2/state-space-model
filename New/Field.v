0f07dc49aefd0a9ab81ac7f984d77a01d75c2364
Eric McCorkle <emc2@metricspace.net>
2013-12-22 14:38:21 -0500

    Added reciprocal multiply inverse axiom

--- New/Field.v
+++ New/Field.v
@@ -8,12 +8,13 @@ Class FieldOps A := {
   recip : A -> A
 }.
 
-Class FieldAxioms A {fops : FieldOps A} := {
-  div_def : forall x y, div x y == mul x (recip y)
-}.
-
 Notation "x / y" := (div x y).
 Notation "/ x" := (recip x).
+
+Class FieldAxioms A {fops : FieldOps A} := {
+  div_def : forall x y, div x y == mul x (recip y);
+  recip_mul_inv : forall a : A, (/ a) * a == 1
+}.
 
 Class FieldNoAssoc A {fops : FieldOps A} := {
   ring_no_assoc_fna :> RingNoAssoc A;
