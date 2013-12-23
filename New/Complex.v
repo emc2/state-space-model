505d4a369d66938b50f7cf3aa75c704f62558051
Eric McCorkle <emc2@metricspace.net>
2013-12-22 22:00:12 -0500

    Split complex into versions with and without ring ops

--- New/Complex.v
+++ New/Complex.v
@@ -9,8 +9,13 @@ Class ComplexOps A := {
 (* Unorthodox notation *)
 Notation "~ x" := (complex_conj x).
 
-Class Complex A {cops : ComplexOps A} {rops : RingOps A} := {
-  conj_inv :> Involution complex_conj equiv_op;
-  conj_sum : forall a b : A, ~(a + b) == (~a) + (~b);
+Class Complex A {cops : ComplexOps A} := {
+  equiv_c :> Equiv A;
+  conj_inv :> Involution complex_conj equiv_op
+}.
+
+Class ComplexRing A {cops : ComplexOps A} {rops : RingOps A} := {
+  complex_cr :> Complex A;
+  conj_sum_cr : forall a b : A, ~(a + b) == (~a) + (~b);
   conj_mul : forall a b : A, ~(a * b) = (~b) * (~a)
 }.
